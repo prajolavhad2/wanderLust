ca83cc794d38fb2cef29eff83dcd95d90782cb70
prajolavhad2 <prajolavhad2@gmail.com>
2026-03-15 00:21:47 +0530

    added category schema to Joi in schema.js 2

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const Joi = require("joi");
+
 module.exports.listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
@@ -7,5 +9,12 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     image: Joi.string().allow("", null),
     category: Joi.string().allow("", null),
+  }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
   }).required(),
 });
